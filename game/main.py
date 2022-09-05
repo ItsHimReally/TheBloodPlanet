b335f0237800dbd7d334574472df84b26feca38d
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2022-09-06 00:45:34 +0300

    игровой цикл пока не работает

---
+++ game/main.py
@@ -0,0 +1,21 @@
+from Editor import Base
+import pygame
+
+
+pygame.init()
+screen = pygame.display.set_mode()
+pygame.mouse.set_visible(0)
+pygame.display.set_caption('')
+
+game = True
+player = Base.SpriteObject('player', None, 'player', Base.Transform(), 'C:/Users\Danya/RedPlanet-Game/sprites/main.png')
+
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+    player.paint(screen)
+    pygame.display.update()
+pygame.quit()
+exit()
+

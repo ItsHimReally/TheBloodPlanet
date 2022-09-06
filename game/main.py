73dcc5c5afc17ffb506d8718096245600d28d636
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2022-09-06 17:17:19 +0300

    Управление игроком
    Бедное? ДА. Без физика? ДА. Управление? ДА. Чего же тебе еще надо

--- game/main.py
+++ game/main.py
@@ -8,17 +8,26 @@ pygame.mouse.set_visible(0)
 pygame.display.set_caption('')
 
 game = True
+# объект игрока
 player = Base.SpriteObject('player', None, 'player', Base.Transform(), 'C:/Users\Danya/RedPlanet-Game/sprites/main.png')
 
 while game:
     screen.fill([0, 0, 0])
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-    player.transform.translate(2)
+    keys = pygame.key.get_pressed()  # клавиши, которые были нажаты
+    # реакции на нажатия клавиш
+    if keys[pygame.K_a]:
+        player.transform.translate(-2)
+    elif keys[pygame.K_d]:
+        player.transform.translate(2)
+    elif keys[pygame.K_w]:
+        player.transform.translate(0, -2)
+
     player.paint(screen)
     pygame.display.update()
+
 pygame.quit()
 exit()
 

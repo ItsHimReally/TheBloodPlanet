d662d54c386bb71f7a28cdab42fbd95d412b9318
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2022-09-06 16:57:24 +0300

    программисты работают
    теперь есть основной игровой цикл и зеленый квадрат, который двигается на 2 пикселя каждую итерацию. И да, работает всё на архитектуре

--- game/main.py
+++ game/main.py
@@ -11,9 +11,12 @@ game = True
 player = Base.SpriteObject('player', None, 'player', Base.Transform(), 'C:/Users\Danya/RedPlanet-Game/sprites/main.png')
 
 while game:
+    screen.fill([0, 0, 0])
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+    player.transform.translate(2)
     player.paint(screen)
     pygame.display.update()
 pygame.quit()
